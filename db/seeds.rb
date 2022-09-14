d81fdd14a51bf87b020f261ddb5562111e0614a9
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-09-14 13:20:32 +0100

    initial seed file setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,38 @@
+
+puts "Deleting old data..."
+Reviewer.destroy_all
+City.destroy_all
+Review.destroy_all
+
 puts "Seeding games..."
 
-# run a loop 50 times
-50.times do
-  # create a game with random data
-  Game.create(
-    title: Faker::Game.title,
-    genre: Faker::Game.genre,
-    platform: Faker::Game.platform,
-    price: rand(0..60) # random number between 0 and 60
+# run a loop 10 times
+10.times do
+  # create a reviewer with random data
+  Reviewer.create(
+    name: Faker::Name.unique.name,
+    bio: Faker::String.string
+  )
+
+  City.create(
+    name: Faker::Address.unique.city
+    minimum_wage: rand(10..100),
+    currency: Faker::Currency.unique.name
+    country: Faker::Address.unique.country
+    employment_rate: rand(0..100)
+  )
+
+  Review.create(
+    comment: Faker::Name.name,
+    traffic: rand(1..5),
+    night_life: rand(1..5),
+    safety: rand(1..5), 
+    friendly_to_foreigner: rand(1..5),
+    places_to_work_from: rand(1..5),
+    quality_of_internet: rand(1..5)
+    quality_of_healthcare: rand(1..5)
+    city_id: rand(1..10)
+    reviewer_id: rand(1..10)
   )
 end
 
