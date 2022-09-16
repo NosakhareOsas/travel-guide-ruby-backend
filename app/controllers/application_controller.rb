a18e12fc5a294bcc777fb6d94963973e161e2cfb
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-09-16 11:32:39 +0100

    add crud and rank routes to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,11 @@ class ApplicationController < Sinatra::Base
         cities.to_json
     end
 
+    get '/cities/:rank' do
+        cities = City.all.order(params[:rank]).reverse
+        cities.to_json
+    end
+
     get '/cities/:id' do
         city = City.find(params[:id])
         city.to_json(include: {
@@ -16,10 +21,47 @@ class ApplicationController < Sinatra::Base
         })
     end
 
-    get '/users/:id' do
+    get '/users' do
         users = User.all
         users.to_json
     end
 
+    post '/reviews' do
+    review = Review.create(
+        comment: params[:comment],
+        traffic: params[:traffic],
+        night_life: params[:night_life],
+        safety: params[:safety],
+        friendly_to_foreigner: params[:friendly_to_foreigner],
+        places_to_work_from: params[:places_to_work_from],
+        quality_of_internet: params[:quality_of_internet],
+        quality_of_healthcare: params[:quality_of_healthcare],
+        city_id: params[:city_id],
+        user_id: params[:user_id]
+    )
+    review.to_json
+    end
+
+    delete '/reviews/:id' do
+        review = Review.find(params[:id])
+        review.destroy
+        review.to_json
+      end
     
+    patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+        comment: params[:comment],
+        traffic: params[:traffic],
+        night_life: params[:night_life],
+        safety: params[:safety],
+        friendly_to_foreigner: params[:friendly_to_foreigner],
+        places_to_work_from: params[:places_to_work_from],
+        quality_of_internet: params[:quality_of_internet],
+        quality_of_healthcare: params[:quality_of_healthcare]
+    )
+    review.to_json
+    end
+
+
 end    
