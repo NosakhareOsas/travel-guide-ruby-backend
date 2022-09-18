0da0b40629d197e5eac2e5ae1bda9edf4e66b947
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-09-18 23:10:50 +0100

    update routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
     get '/cities/:id' do
         city = City.find(params[:id])
         city.to_json(include: {
-            reviews: {only: [:comment, :traffic, :night_life, :friendly_to_foreigner, :places_to_work_from, :quality_of_healthcare, :quality_of_internet], include: {
+            reviews: {only: [:comment, :traffic, :night_life, :friendly_to_foreigner, :places_to_work_from, :quality_of_healthcare, :quality_of_internet, :updated_at, :id], include: {
                 user: { only: [:name]}
             }}
         })
